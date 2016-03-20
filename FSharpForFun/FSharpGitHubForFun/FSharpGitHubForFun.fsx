e5b29a07c4af739ae599cd37f00a923a142f8627
Raul Arrieta Brouard <raul.arrieta@gmail.com>
2016-03-20 19:27:24 +0100

    added F# for fun
    Just few experiments using Octokit to retrieve git repo commit history
    and analyze changes

---
+++ FSharpForFun/FSharpGitHubForFun/FSharpGitHubForFun.fsx
@@ -0,0 +1,96 @@
+﻿#r "../packages/Octokit.0.19.0/lib/net45/Octokit.dll"
+#r "../packages/FSharp.Configuration.0.5.12/lib/net40/FSharp.Configuration.dll"
+#r "System.Configuration.dll"
+#r "System.Core.dll"
+#r "System.dll"
+
+open System;
+open System.Collections.Generic
+open System.Threading.Tasks
+open FSharp.Configuration
+open Octokit
+
+type CommitInfo = {Hash : string; Author : string; TimeStamp : DateTime; Message : string}
+type CommittedFile = {LinesAdded: int option; LinesDeleted: int option; FileName: string}
+type Commit = {CommitInfo: CommitInfo; Files: CommittedFile[]}
+
+type Settings = AppSettings<"app.config">
+
+
+let getCommit (client : GitHubClient) user repo ref = 
+    async { 
+        return! 
+            client.Repository.Commit.Get(user, repo, ref) 
+            |> Async.AwaitTask 
+    }
+
+let getCommits (client : GitHubClient) user repo = 
+    async { 
+        let! 
+            commits = 
+            client.Repository.Commit.GetAll(user, repo)
+            |> Async.AwaitTask
+
+        return! 
+            commits
+            |> Seq.map (fun c -> getCommit client c.Author.Login repo c.Sha)
+            |> Async.Parallel
+    }
+
+let extractFileInfo (file : GitHubCommitFile) = 
+     {
+        LinesAdded = Some(file.Additions); 
+        LinesDeleted = Some(file.Deletions); 
+        FileName = file.Filename
+     }
+
+let extractCommitInfo (commit : GitHubCommit) =
+    {
+        CommitInfo = {Hash = commit.Commit.Sha.ToString(); Author = commit.Commit.Author.Name; TimeStamp = commit.Commit.Committer.Date.DateTime; Message = commit.Commit.Message}; 
+        Files = 
+            commit.Files
+            |> Seq.toArray
+            |> Array.map extractFileInfo
+    }
+
+
+
+let gitHubClient = new GitHubClient( new ProductHeaderValue(Settings.RepositoryHeader), Settings.RepositoryUri);
+gitHubClient.Credentials = Credentials(Settings.RepositoryToken);
+
+let commits = 
+    getCommits gitHubClient Settings.RepositoryOwner Settings.RepositoryHeader
+    |> Async.RunSynchronously
+
+let totalCommits = 
+    commits
+    |> Array.map extractCommitInfo;
+
+let numberOfCommits = 
+    totalCommits 
+    |> Array.length
+
+let numberOfEntitiesChanged =
+    totalCommits
+    |> Array.collect(fun c -> c.Files)
+    |> Array.length
+
+let numberOfEntities = 
+    totalCommits
+    |> Array.collect(fun c -> c.Files)
+    |> Array.groupBy(fun f -> f.FileName)
+    |> Array.length
+    
+let numberOfAuthors =
+    totalCommits
+    |> Array.groupBy(fun c -> c.CommitInfo.Author)
+    |> Array.distinct
+    |> Array.length
+
+let numberOfRevisions =
+    totalCommits
+    |> Array.collect(fun c -> c.Files)
+    |> Array.groupBy(fun f -> f.FileName)
+    |> Array.map ( fun c -> fst c, (snd c).Length)
+    |> Array.sortByDescending ( fun c -> snd c)
+    
--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Curiosity
 [![Build Status](https://travis-ci.org/raul-arrieta/curiosity.svg?branch=Sharding-Pattern)](https://travis-ci.org/raul-arrieta/curiosity)
 ## Sharding Pattern
-Divide a data store into a set of horizontal partitions or shards. This pattern can improve scalability when storing and accessing large volumes of data.
+Divide a data store into a set of horizontal partitions or shards. 
+This pattern can improve scalability when storing and accessing large volumes of data.
 http://nehola.com/5t
 
 ## ASP.NET SignalR Example
-DASP.NET SignalR is a new library for ASP.NET developers that makes developing real-time web functionality easy.
+ASP.NET SignalR is a new library for ASP.NET developers that makes developing real-time web functionality easy.
 SignalR allows bi-directional communication between server and client. Servers can now push content to connected clients instantly as it becomes available. 
 http://nehola.com/6d
+
+## F# for Fun
+Just few experiments using Octokit to retrieve git repo commit history and analyze changes.
+Octokit - https://github.com/octokit
